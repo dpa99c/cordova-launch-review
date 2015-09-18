19293e43a5a169481c6baac5d9f06dad332e6760
Dave Alden <dpa99c@gmail.com>
2015-09-18 16:11:38 +0100

    Add copyright license

--- www/launchreview.js
+++ www/launchreview.js
@@ -1,16 +1,36 @@
-
-var exec = require('cordova/exec');
-
+/*
+ * Copyright (c) 2015 Dave Alden  (http://github.com/dpa99c)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
 var LaunchReview = function() {};
 
 /**
- * Launches iTunes store on review page in order to leave a review for given app
- * @param appId Apple ID of app to open in iTunes Store
- * @param successCallback function to be called when plugin call was successful
- * @param errorCallback function to be called if the plugin call failed
+ * Launches App Store on current platform in order to leave a review for given app
+ * @param {string} ID of app to open in App Store
+ * @param {function} successCallback function to be called when plugin call was successful
  */
-LaunchReview.prototype.launch = function(appId, successCallback,failureCallback) {
-    exec(successCallback, failureCallback, 'LaunchReview', 'launch', [appId]);
-}
-var launchreview = new LaunchReview();
-module.exports = launchreview;
+LaunchReview.prototype.launch = function(appId, successCallback) {
+    cordova.exec(successCallback, null, 'LaunchReview', 'launch', [appId]);
+};
+module.exports = new LaunchReview();
